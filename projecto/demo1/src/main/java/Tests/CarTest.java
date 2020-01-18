a4102a3baf37e563095d38966ed335750cb626e3
Paulo Gameiro <pmngameiro@gmail.com>
2020-01-18 23:15:27 +0000

    Tests

---
+++ projecto/demo1/src/main/java/Tests/CarTest.java
@@ -0,0 +1,80 @@
+package Tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    @Test
+    void setPosition() {
+    }
+
+    @Test
+    void setBasePrice() {
+    }
+
+    @Test
+    void getOwnerID() {
+    }
+
+    @Test
+    void getAvgSpeed() {
+    }
+
+    @Test
+    void getBasePrice() {
+    }
+
+    @Test
+    void getPosition() {
+    }
+
+    @Test
+    void getRange() {
+    }
+
+    @Test
+    void getType() {
+    }
+
+    @Test
+    void getNumberPlate() {
+    }
+
+    @Test
+    void isAvailable() {
+    }
+
+    @Test
+    void swapState() {
+    }
+
+    @Test
+    void refil() {
+    }
+
+    @Test
+    void hasRange() {
+    }
+
+    @Test
+    void rate() {
+    }
+
+    @Test
+    void testRate() {
+    }
+
+    @Test
+    void pendingRental() {
+    }
+
+    @Test
+    void approvePendingRental() {
+    }
+
+    @Test
+    void testClone() {
+    }
+}
---
+++ projecto/demo1/src/main/java/Tests/CarsTest.java
@@ -0,0 +1,32 @@
+package Tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarsTest {
+
+    @Test
+    void addCar() {
+    }
+
+    @Test
+    void searchCar() {
+    }
+
+    @Test
+    void listOfCarType() {
+    }
+
+    @Test
+    void getCar() {
+    }
+
+    @Test
+    void testGetCar() {
+    }
+
+    @Test
+    void testGetCar1() {
+    }
+}
