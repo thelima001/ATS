ff62feff19e58e50f8d675958ba418aed398efae
Sofia <sofia@pop-os.localdomain>
2020-01-20 20:09:07 +0000

    variaveis em vez de repetiçoes

---
+++ projecto/variaveisEmVezDeRepetiçoes/src/main/java/model/Weather.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+
+class Weather {
+    private Random random = new Random();
+    private static String fall = "Fall";
+    private static String winter = "Winter";
+    private static String spring = "Spring";
+    private static String summer = "Summer";
+    		
+    private static final String[] seasons = {
+    		winter, winter,
+    		spring, spring, spring,
+    		summer, summer, summer,
+            fall, fall, fall,
+            winter
+    };
+    private String getSeason() {
+        return seasons[LocalDateTime.now().getMonthValue()];
+    }
+
+    public double getSeasonDelay() {
+        switch (getSeason()){
+            case "Summer":
+                return this.random.nextDouble() % 0.1;
+
+            case "Spring":
+                return this.random.nextDouble() % 0.3;
+
+            case "Fall":
+                return this.random.nextDouble() % 0.35;
+
+            default:
+                return this.random.nextDouble() % 0.6;
+        }
+    }
+}
