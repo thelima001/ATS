edd9110326b17ea7e911657511b050252fbb8d74
Sofia <sofia@pop-os.localdomain>
2020-01-20 19:32:40 +0000

    noBlockers

---
+++ projecto/noBlockers/src/main/java/Model/Traffic.java
@@ -0,0 +1,20 @@
+package Model;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+
+class Traffic {
+
+    private Random random  = new Random();
+
+    public double getTraficDelay(double delay) {
+        int a = LocalDateTime.now().getHour();
+
+        if(a == 18 || a == 8)
+            return (this.random.nextDouble() % 0.6) + (delay % 0.2);
+        if(a > 1 && a < 6)
+            return (this.random.nextDouble() % 0.1) + (delay % 0.2);
+        return (this.random.nextDouble() % 0.3) + (delay % 0.2);
+    }
+
+}
---
+++ projecto/noBlockers/src/main/java/Model/Weather.java
@@ -0,0 +1,36 @@
+package Model;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+
+class Weather {
+    private Random random = new Random();
+    private static final String[] seasons = {
+            "Winter", "Winter",
+            "Spring", "Spring", "Spring",
+            "Summer", "Summer", "Summer",
+            "Fall", "Fall", "Fall",
+            "Winter"
+    };
+
+    private String getSeason() {
+        return seasons[LocalDateTime.now().getMonthValue()];
+    }
+
+    public double getSeasonDelay() {
+
+        switch (getSeason()){
+            case "Summer":
+                return this.random.nextDouble() % 0.1;
+
+            case "Spring":
+                return this.random.nextDouble() % 0.3;
+
+            case "Fall":
+                return this.random.nextDouble() % 0.35;
+
+            default:
+                return this.random.nextDouble() % 0.6;
+        }
+    }
+}
